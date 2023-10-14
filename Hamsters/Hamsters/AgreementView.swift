45570b3bda44742f6a1aaa5e335db5895fd3d38a
ChaeeunShin <shince0805@gmail.com>
2023-10-15 00:40:33 +0900

    [Feat/#4] 약관 동의 페이지(AgreementView) UI 구현

---
+++ Hamsters/Hamsters/AgreementView.swift
@@ -0,0 +1,18 @@
+//
+//  AgreementView.swift
+//  Hamsters
+//
+//  Created by Chaeeun Shin on 10/14/23.
+//
+
+import SwiftUI
+
+struct AgreementView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AgreementView()
+}
---
+++ Hamsters/Hamsters/Scenes/Onboarding/NextButton.swift
@@ -0,0 +1,18 @@
+//
+//  NextButton.swift
+//  Hamsters
+//
+//  Created by Chaeeun Shin on 10/14/23.
+//
+
+import SwiftUI
+
+struct NextButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NextButton()
+}
