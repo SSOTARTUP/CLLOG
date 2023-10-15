d894cdfdb07c624d944c3b86ff5cf0cdbecf9e5c
ChaeeunShin <shince0805@gmail.com>
2023-10-15 21:14:45 +0900

    [Feat/#4] 닉네임 입력 페이지(NicknameView) UI 구현

---
+++ Hamsters/Hamsters/Scenes/Onboarding/NicknameView.swift
@@ -0,0 +1,97 @@
+//
+//  NicknameView.swift
+//  Hamsters
+//
+//  Created by Chaeeun Shin on 10/15/23.
+//
+
+import SwiftUI
+
+struct NicknameView: View {
+    @Binding var pageNumber: Int
+    @State private var nickname = ""
+    @State private var isActiveNext = false
+    @State private var focusField = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 0) {
+                OnboardinProgressBar(pageNumber: $pageNumber)
+                    .padding(.bottom, 15)
+                
+                Group {
+                    Text("닉네임을 알려주세요!")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.bottom, 29)
+                    
+                    Text("닉네임 입력")
+                        .font(.subheadline)
+                        .foregroundStyle(.thoNavy)
+                        .opacity(focusField ? 1 : 0)
+                        .padding(.bottom, 6)
+                }
+                .padding(.horizontal, 24)
+                
+                TextField("닉네임 입력", text: $nickname)
+                    .textInputAutocapitalization(.never)
+                    .disableAutocorrection(true)
+                    .padding(.vertical, 11)
+                    .padding(.leading, 8)
+                    .background(Color(uiColor: .secondarySystemBackground))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 16)
+                    .onTapGesture {
+                        focusField = true
+                    }
+                    .onChange(of: nickname) { _ in
+                        if nickname.count == 0 {
+                            isActiveNext = false
+                        } else {
+                            isActiveNext = true
+                        }
+
+                        if nickname.count > 5 {
+                            nickname = String(nickname.prefix(5))
+                        }
+                    }
+                    .onSubmit {
+                        focusField = false
+                    }
+
+                Spacer()
+
+                Button {
+                    
+                } label: {
+                    NextButton(isActive: $isActiveNext, pageNumber: $pageNumber)
+                        .padding(.bottom, 30)
+                }
+                .disabled(!isActiveNext)
+                .padding(.horizontal, 24)
+                .padding(.bottom, 30)
+            }
+            .onTapGesture {
+                hideKeyboard()
+                focusField = false
+            }
+            
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "chevron.backward")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.thoNavy)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    NicknameView(pageNumber: .constant(1))
+}
