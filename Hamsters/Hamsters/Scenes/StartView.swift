a7f2854b71ff2cfd43a121d62a48772af738d98d
ChaeeunShin <shince0805@gmail.com>
2023-10-16 11:18:48 +0900

    [Feat/#4] 앱 시작 시 분기점이 되는 뷰 임시 구현

---
+++ Hamsters/Hamsters/Scenes/StartView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Hamsters
+//
+//  Created by Chaeeun Shin on 10/14/23.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        ZStack {
+            OnboardingView()
+        }
+    }
+}
+
+#Preview {
+    StartView()
+}
