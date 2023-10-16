c10e23e0cb7f7137d855953f0048f7ac44a705e2
ChaeeunShin <shince0805@gmail.com>
2023-10-16 23:41:58 +0900

    [Feat/#8] UserDefaultsKey 및 유저 입력 사항 enum 처리

---
+++ Hamsters/Hamsters/Models/Enums/SexClassification.swift
@@ -0,0 +1,14 @@
+//
+//  SexClassification.swift
+//  Hamsters
+//
+//  Created by Chaeeun Shin on 10/16/23.
+//
+
+import Foundation
+
+enum SexClassification: String {
+    case female
+    case male
+    case menopause
+}
