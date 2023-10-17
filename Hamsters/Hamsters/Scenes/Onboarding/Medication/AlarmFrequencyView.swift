34a3f804ae52339e4c5141cf1690e5d0117fb1a0
Ryu <ywg6525@daum.net>
2023-10-18 06:30:14 +0900

    [Feat/#5] 빈도 설정 요일선택, 시작일 구현

--- Hamsters/Hamsters/Scenes/Onboarding/Medication/AlarmFrequencyView.swift
+++ Hamsters/Hamsters/Scenes/Onboarding/Medication/AlarmFrequencyView.swift
@@ -14,40 +14,89 @@ struct AlarmFrequencyView: View {
     }
     
     @State private var selectedOption: Option? = nil
+    @Binding var selectedDays: [Day]
+    @Binding var startDay: Date
     
     var body: some View {
         
         VStack(alignment: .leading, spacing: 0) {
             
-            ForEach(Array(zip(0..<Option.allCases.count, Option.allCases)), id: \.0) { index, option in
-                
-                if index > 0 {
-                    Divider()
-                }
-                
-                HStack {
-                    Text("\(option.rawValue)")
-                        .font(.body)
+            VStack(alignment: .leading, spacing: 0) {
+                ForEach(Array(zip(0..<Option.allCases.count, Option.allCases)), id: \.0) { index, option in
                     
-                    Spacer()
+                    if index > 0 {
+                        Divider()
+                    }
                     
-                    if selectedOption == option {
-                        Image(systemName: "checkmark")
-                            .font(.headline)
-                            .foregroundStyle(.blue)
+                    HStack {
+                        Text("\(option.rawValue)")
+                            .font(.body)
+                        
+                        Spacer()
+                        
+                        if selectedOption == option {
+                            Image(systemName: "checkmark")
+                                .font(.headline)
+                                .foregroundStyle(.blue)
+                        }
+                    }
+                    .padding(.vertical, 14)
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        selectedOption = option
                     }
                 }
-                .padding(.vertical, 14)
-                .contentShape(Rectangle())
-                .onTapGesture {
-                    selectedOption = option
-                }
+                .padding(.horizontal, 16)
+                .background(.thoTextField)
             }
-            .padding(.horizontal, 16)
-            .background(.thoTextField)
+            .cornerRadius(10)
+            .padding(.top, 24)
+            .padding(.bottom, 44)
             
+            if selectedOption == Option.specificDay {
+                Text("요일 선택")
+                    .font(.headline)
+                    .foregroundStyle(.thoNavy)
+                    .padding(.leading, 8)
+                    .padding(.bottom, 6)
+                
+                HStack(spacing: 4) {
+                    ForEach(Day.allCases, id: \.self) { day in
+                        Text(String(day.rawValue.first!))
+                            .font(.title3)
+                            .foregroundStyle(selectedDays.contains(day) ? Color.white : Color.black)
+                            .padding(14)
+                            .background(
+                                Circle()
+                                    .fill(selectedDays.contains(day) ? Color.thoNavy : Color.clear)
+                            )
+                        
+                            .onTapGesture {
+                                if selectedDays.contains(day) {
+                                    selectedDays.removeAll(where: {$0 == day})
+                                } else {
+                                    selectedDays.append(day)
+                                }
+                            }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal, 16)
+                .background(Color.thoTextField)
+                .cornerRadius(10)
+                .padding(.bottom, 44)
+            }
+            
+            DatePicker("시작일", selection: $startDay, displayedComponents: .date)
+                .font(.body)
+                .padding(.horizontal, 8)
+                .padding(.vertical, 11)
+                .background(Color.thoTextField)
+                .cornerRadius(10)
+            
+            
+            Spacer()
         }
-        .cornerRadius(10)
         .padding(.horizontal, 16)
         
         
@@ -56,5 +105,5 @@ struct AlarmFrequencyView: View {
 }
 
 #Preview {
-    AlarmFrequencyView()
+    AlarmFrequencyView(selectedDays:.constant([.monday, .wednesday]), startDay: .constant(Date()))
 }
