61b868ebf2f3e27ee78aa1f27ca49cdcb8695c92
ChaeeunShin <shince0805@gmail.com>
2023-10-18 20:26:26 +0900

    [Feat/#14] 컨디션 및 기분 체크 슬라이더 컴포넌트 UI

---
+++ Hamsters/Hamsters/Scenes/DailyRecord/Components/ConditionSlider.swift
@@ -0,0 +1,90 @@
+//
+//  ConditionSlider.swift
+//  Hamsters
+//
+//  Created by Chaeeun Shin on 10/18/23.
+//
+
+import SwiftUI
+import Sliders
+
+struct ConditionSlider: View {
+    let title: String
+    @State private var userValue = 0.0
+    @State private var range = 0.0...4.0
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(title)
+                .font(.body)
+                .padding(.horizontal, 12)
+            
+            ValueSlider(value: $userValue, in: range, step: 1.0)
+                .valueSliderStyle(
+                    HorizontalValueSliderStyle(
+                        track:
+                            HorizontalValueTrack(
+                                view: Capsule()
+                                        .foregroundStyle(.thoNavy)
+                                        .padding(.horizontal, 4)
+                            )
+                            .background {
+                                Capsule()
+                                    .foregroundStyle(.thoDisabled)
+                                    .padding(.horizontal, 4)
+                            }
+                            .frame(height: 12),
+                        thumb:
+                            Circle()
+                            .frame(width: 48, height: 48)
+                            .foregroundStyle(.white)
+                            .shadow(color: .black.opacity(0.2), radius: 3, x: 0.0, y: 1.0)
+                            .overlay{
+                                Image("MyStatus"+String(Int(userValue)))
+                                    .resizable()
+                                    .scaledToFit()
+                                    .padding(.top, 6)
+                                    .padding(.bottom, 10)
+                            },
+                        thumbSize: CGSize(width: 56, height: 56)
+                    )
+                )
+                .frame(height: 64)
+                .clipped()
+                .padding(.horizontal, 8)
+            
+            HStack {
+                Text("괜찮음")
+                
+                Spacer()
+                
+                Text("|")
+                    .foregroundStyle(.secondary)
+
+                Spacer()
+                
+                Text("중간")
+                
+                Spacer()
+                
+                Text("|")
+                    .foregroundStyle(.secondary)
+                
+                Spacer()
+                
+                Text("심했음")
+            }
+            .font(.footnote)
+            .foregroundStyle(.thoNavy)
+            .padding(.horizontal, 18)
+        }
+        .padding(.vertical, 12)
+        .background(Color(uiColor: .secondarySystemBackground))
+        .cornerRadius(15)
+        .padding(.horizontal, 16)
+    }
+}
+
+#Preview {
+    ConditionSlider(title: "집중하기 어려움")
+}
