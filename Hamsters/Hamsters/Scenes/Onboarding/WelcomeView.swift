95019d21571a0460c2bdc05a1e5cb13d162ed6e4
jesik <vuswotlr@gmail.com>
2023-11-08 18:46:25 +0900

    filec

---
+++ Hamsters/Hamsters/Scenes/Onboarding/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  Hamsters
+//
+//  Created by jaesik pyeon on 11/8/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct WelcomeView:View{
+    var body:some View{
+        Text("hi")
+    }
+}
+#Preview{
+    WelcomeView()
+}
