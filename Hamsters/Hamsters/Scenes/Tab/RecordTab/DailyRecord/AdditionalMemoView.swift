77f993298c1bf87652e8f969c3607fe86a2e7fc9
ChaeeunShin <shince0805@gmail.com>
2023-11-12 23:58:32 +0900

    [Feat/#61] 글자수 제한 기능 추가

--- Hamsters/Hamsters/Scenes/Tab/RecordTab/DailyRecord/AdditionalMemoView.swift
+++ Hamsters/Hamsters/Scenes/Tab/RecordTab/DailyRecord/AdditionalMemoView.swift
@@ -14,28 +14,47 @@ struct AdditionalMemoView: View {
     let placeholder = "있다면 자유롭게 입력해 주세요"
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Text("추가로 기록하고싶은\n내용이 있나요? ✍️️")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding(.bottom, 16)
-                .padding(.horizontal, 16)
-            
-            TextEditor(text: $memo)
-                .scrollContentBackground(.hidden)
-                .background {
-                    TextEditor(text: .constant(memo.isEmpty ? placeholder : ""))
-                        .foregroundStyle(.gray)
-                        .scrollContentBackground(.hidden)
-                        .background(Color(uiColor: .secondarySystemBackground))
-                }
-//                .background(Color(uiColor: .secondarySystemBackground))
-                .cornerRadius(10)
-                .padding(.horizontal, 16)
+        VStack(alignment: .leading, spacing: 0) {
+            Group {
+                Text("추가로 기록하고싶은\n내용이 있나요? ✍️️")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 16)
                 
+                TextEditor(text: $memo)
+                    .scrollContentBackground(.hidden)
+                    .background {
+                        TextEditor(text: .constant(memo.isEmpty ? placeholder : ""))
+                            .foregroundStyle(.gray)
+                            .scrollContentBackground(.hidden)
+                            .background(Color(uiColor: .secondarySystemBackground))
+                    }
+                    .frame(height: screenBounds().height * 0.35)
+                    .cornerRadius(10)
+                    .padding(.top, 20)
+                    .onChange(of: memo) { _ in
+                        if memo.count > 500 {
+//                            characterLimitWarning = true
+                            memo = String(memo.prefix(7))
+                        } else if memo.count < 7 {
+//                            characterLimitWarning = false
+                        }
+                    }
+                
+                HStack {
+                    Spacer()
+                    
+                    Text("\(memo.count) / 500")
+                        .font(.body)
+                        .foregroundStyle(.secondary)
+                }
+                .padding(.top, 8)
+            }
+            .padding(.horizontal, 16)
+
             Spacer()
             
-            DailyRecordNextButton(pageNumber: $pageNumber, isActiveRecord:.constant(true), title: "입력 완료")
+            DailyRecordNextButton(pageNumber: $pageNumber, isActiveRecord:.constant(true), title: "\(Image(systemName: "checkmark.circle.fill")) 입력 완료")
         }
     }
 }
