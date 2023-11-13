f388bda9cd95fdbb0859a80a8e1eb4ea4f428325
ChaeeunShin <shince0805@gmail.com>
2023-11-13 13:32:03 +0900

    [Design/#61] 음주량 입력 페이지 디자인 변경

--- Hamsters/Hamsters/Scenes/Tab/RecordTab/DailyRecord/DrinkCheckView.swift
+++ Hamsters/Hamsters/Scenes/Tab/RecordTab/DailyRecord/DrinkCheckView.swift
@@ -20,27 +20,17 @@ struct DrinkCheckView: View {
                 .padding(.horizontal, 16)
             
             List {
-                ForEach(DrinkAmount.allCases, id: \.self) { bottle in
-                    if bottle.maxValue != 0 {
+                Section {
+                    Button {
+                        amountOfAlcohol = 0
+                    } label: {
                         HStack {
-                            Button {
-                                amountOfAlcohol = bottle.maxValue
-                            } label: {
-                                if bottle.maxValue == 5 {
-                                    Text("\(bottle.maxValue)병 이상")
-                                        .foregroundStyle(Color.primary)
-                                } else if bottle.maxValue == 1 {
-                                    Text("\(bottle.maxValue)병 미만")
-                                        .foregroundStyle(Color.primary)
-                                } else {
-                                    Text("\(bottle.minValue)~\(bottle.maxValue) 병")
-                                        .foregroundStyle(Color.primary)
-                                }
-                            }
+                            Text("오늘은 마시지 않았어요!")
+                                .foregroundStyle(Color.primary)
                             
                             Spacer()
                             
-                            if amountOfAlcohol == bottle.maxValue {
+                            if amountOfAlcohol == 0 {
                                 Image(systemName: "checkmark")
                                     .fontWeight(.bold)
                                     .foregroundStyle(.thoNavy)
@@ -49,6 +39,30 @@ struct DrinkCheckView: View {
                     }
                 }
                 .listRowBackground(Color(uiColor: .systemGroupedBackground))
+                
+                Section(footer: Text("음주량은 소주를 기준으로 합니다.")) {
+                    ForEach(DrinkAmount.allCases, id: \.self) { bottle in
+                        if bottle != .max0 {
+                            HStack {
+                                Button {
+                                    amountOfAlcohol = bottle.maxValue
+                                } label: {
+                                    Text(bottle.title)
+                                        .foregroundStyle(Color.primary)
+                                }
+                                
+                                Spacer()
+                                
+                                if amountOfAlcohol == bottle.maxValue {
+                                    Image(systemName: "checkmark")
+                                        .fontWeight(.bold)
+                                        .foregroundStyle(.thoNavy)
+                                }
+                            }
+                        }
+                    }
+                    .listRowBackground(Color(uiColor: .systemGroupedBackground))
+                }
             }
             .padding(.top, -15) // list 기본 padding 제거
             .scrollContentBackground(.hidden)
