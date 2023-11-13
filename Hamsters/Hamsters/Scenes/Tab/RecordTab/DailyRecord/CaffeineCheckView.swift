8d7491f8639c58f0e3b1f06b2907e5909d84b41b
ChaeeunShin <shince0805@gmail.com>
2023-11-13 12:24:41 +0900

    [Feat/#61] 카페인 입력 페이지 구현

--- Hamsters/Hamsters/Scenes/Tab/RecordTab/DailyRecord/CaffeineCheckView.swift
+++ Hamsters/Hamsters/Scenes/Tab/RecordTab/DailyRecord/CaffeineCheckView.swift
@@ -7,10 +7,17 @@
 
 import SwiftUI
 
+enum CaffeineIntake {
+    case intake
+    case not
+}
+
 struct CaffeineCheckView: View {
     @Binding var pageNumber: Int
     @Binding var amountOfCaffein: Int
-    @Binding var isSelected: [Bool]
+    
+    @State private var isSelected: [Bool] = Array(repeating: false, count: 10)
+    @State private var isTaken: CaffeineIntake?
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
@@ -19,23 +26,65 @@ struct CaffeineCheckView: View {
                 .fontWeight(.bold)
                 .padding(.bottom, 16)
                 .padding(.horizontal, 16)
-
-            LazyVGrid(columns: Array(repeating: .init(.flexible()), count: 5)) {
-                ForEach(0..<10, id: \.self) { index in
-                    Button {
-                        if isSelected[index] {
-                            isSelected = Array(repeating: false, count: 10)
-                        }
-                        for i in 0...index {
-                            isSelected[i] = true
-                        }
-                        amountOfCaffein = index
-                    } label: {
-                        Image(isSelected[index] ? "CaffeineSelected" : "CaffeineUnselected")
-                    }
+            
+            HStack(spacing: 13) {
+                Button {
+                    isTaken = .intake
+                } label: {
+                    Text("마심")
+                        .font(.headline)
+                        .foregroundStyle(isTaken == .intake ? .white : .thoNavy)
+                        .padding(.vertical, 15)
+                        .frame(maxWidth: .infinity)
+                        .background(isTaken == .intake ? .thoNavy : .thoDisabled)
+                        .cornerRadius(15)
+                }
+                
+                Button {
+                    isTaken = .not
+                    amountOfCaffein = 0
+                } label: {
+                    Text("안마심")
+                        .font(.headline)
+                        .foregroundStyle(isTaken == .not ? .white : .thoNavy)
+                        .padding(.vertical, 15)
+                        .frame(maxWidth: .infinity)
+                        .background(isTaken == .not ? .thoNavy : .thoDisabled)
+                        .cornerRadius(15)
                 }
             }
-            .padding(.horizontal, 16)
+            .padding(.horizontal, 24)
+            .padding(.vertical, 32)
+
+            if isTaken == .intake {
+                VStack(alignment: .leading, spacing: 16) {
+                    Text("얼마나 드셨나요?")
+                        .font(.headline)
+                        .foregroundStyle(.thoNavy)
+                    
+                    LazyVGrid(columns: Array(repeating: .init(.flexible()), count: 5)) {
+                        ForEach(0..<10, id: \.self) { index in
+                            Button {
+                                if isSelected[index] {
+                                    isSelected = Array(repeating: false, count: 10)
+                                }
+                                if amountOfCaffein == (index + 1) {
+                                    amountOfCaffein = 0
+                                } else {
+                                    for i in 0...index {
+                                        isSelected[i] = true
+                                    }
+                                    amountOfCaffein = index + 1
+                                }
+                            } label: {
+                                Image(isSelected[index] ? "CaffeineSelected" : "CaffeineUnselected")
+                                    .opacity(isSelected[index] ? 1 : 0.5)
+                            }
+                        }
+                    }
+                }
+                .padding(.horizontal, 16)
+            }
             
             Spacer()
             
@@ -45,5 +94,5 @@ struct CaffeineCheckView: View {
 }
 
 #Preview {
-    CaffeineCheckView(pageNumber: .constant(8), amountOfCaffein: .constant(0), isSelected: .constant(Array(repeating: false, count: 10)))
+    CaffeineCheckView(pageNumber: .constant(8), amountOfCaffein: .constant(0))
 }
