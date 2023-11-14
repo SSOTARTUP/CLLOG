60c86ddaee9388a4ebec39bec7dd01e5e1f53c82
Ryu <ywg6525@daum.net>
2023-11-14 23:42:09 +0900

    [Feat/#78] 부작용 Enum 주요, 위험 부작용으로 따로

--- Hamsters/Hamsters/Models/Enums/SideEffects.swift
+++ Hamsters/Hamsters/Models/Enums/SideEffects.swift
@@ -7,30 +7,41 @@
 
 import Foundation
 
-enum SideEffects: String, CaseIterable {
-    case none = "없음"
-    case headache = "두통"
-    case stomachache = "복통"
-    case noAppetite = "식욕 감소"
-    case insomnia = "불면"
-    case dizzy = "어지러움"
-    case sleepy = "졸림"
-    case sensitive = "신경과민"
-    case cough = "기침"
-    case stomachIssues = "위장장애"
-    case hairLoss = "탈모"
-    case itchy = "가려움"
-    case rash = "발진"
-    case jointPain = "관절통"
-    case fever = "발열"
-    case hives = "두드러기"
-    case nauseous = "구역"
-    case heartRacing = "두근거림"
-    case unsteady = "운동이상"
-    case irregularPulse = "맥박 이상"
-    case tired = "피로"
-    case throatAche = "인후통"
-    case constipated = "변비"
-    case diarrhea = "설사"
-    case others = "기타"
+enum SideEffects {
+    enum Major: String, CaseIterable {
+        case none = "없음"
+        case insomnia = "불면증"
+        case dizzy = "어지러움"
+        case noAppetite = "식욕 감소"
+        case headache = "두통"
+        case stomachache = "복통"
+        case sensitive = "신경과민"
+        case cough = "기침"
+        case stomachIssues = "위장장애"
+        case hairLoss = "탈모"
+        case itchy = "가려움"
+        case rash = "발진"
+        case jointPain = "관절통"
+        case fever = "발열"
+        case nauseous = "구역"
+        case constipated = "변비"
+        case mouthDry = "입마름"
+    }
+    
+    enum Dangerous: String, CaseIterable {
+        case none = "없음"
+        case chestPain = "흉통"
+        case shortnessOfBreath = "호흡곤란"
+        case fainting = "실신"
+        case hallucinations = "환각"
+        case auditoryHallucinations = "환청"
+        case suicidalThoughts = "자살충동"
+        case anxiety = "불안"
+        case restlessness = "초조"
+        case mania = "조증"
+        case fidgeting = "안절부절"
+        case depression = "우울"  
+    }
 }
+
+
