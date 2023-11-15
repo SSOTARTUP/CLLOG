cf85e51feaef2397287680a285137580172efda2
Ryu <ywg6525@daum.net>
2023-11-15 10:51:48 +0900

    [Design/#85] 체중 기록 UI 변경

--- Hamsters/Hamsters/Scenes/Tab/RecordTab/DailyRecord/WeightCheckView.swift
+++ Hamsters/Hamsters/Scenes/Tab/RecordTab/DailyRecord/WeightCheckView.swift
@@ -10,77 +10,87 @@ import SwiftUI
 struct WeightCheckView: View {
     @Binding var pageNumber: Int
     
-    @Binding var selectedKg: Int
-    @Binding var selectedGr: Int
+    @State var selectedKg: Int = 50
+    @State var selectedGr: Int = 0
+    
+    @Binding var weight: Double
     
     var kgRange: [Int] = Array(1...200)
     var grRange: [Int] = Array(0...9)
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-                Group {
-                    Text("체중을 알려주세요")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .padding(.bottom, 12)
-                    
-                    Text("약물 복용으로 인한 체중변화가 있을 수 있습니다.")
-                        .font(.body)
-                        .foregroundStyle(.secondary)
-                        .padding(.bottom, 16)
-                }
-                .padding(.horizontal, 16)
+            Group {
+                Text("체중을 알려주세요")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 12)
                 
-                // .tag picker 에 값 동기화를 위함, 이유 더 찾아보기
-                HStack {
-                    Picker(selection: $selectedKg, label: Text("Kilograms")) {
-                        ForEach(kgRange, id: \.self) { kg in
-                            Text("\(kg)").tag(kg)
-                                .font(.title)
-                                .bold()
-                        }
+                Text("약물 복용으로 인한 체중변화가 있을 수 있습니다.")
+                    .font(.body)
+                    .foregroundStyle(.secondary)
+                    .padding(.bottom, 32)
+            }
+            .padding(.horizontal, 16)
+            
+            HStack {
+                Text("\(selectedKg)" + "." + "\(selectedGr)" + " kg")
+                    .font(.largeTitle)
+                    .foregroundStyle(Color.thoGreen)
+                    .padding(.vertical, 12)
+                    .padding(.horizontal, 28)
+                    .background(Color.thoNavy)
+                    .cornerRadius(41)
+            }
+            .frame(maxWidth: .infinity)
+            
+            // .tag picker 에 값 동기화를 위함, 이유 더 찾아보기
+            HStack {
+                Picker(selection: $selectedKg, label: Text("Kilograms")) {
+                    ForEach(kgRange, id: \.self) { kg in
+                        Text("\(kg)").tag(kg)
+                            .font(.title3)
                     }
-                    .pickerStyle(.wheel)
-                    
-                    Text(".") // 구분점
-                    
-                    Picker(selection: $selectedGr, label: Text("Grams")) {
-                        ForEach(grRange, id: \.self) { gram in
-                            Text("\(gram)").tag(gram)
-                                .font(.title)
-                                .bold()
-                        }
+                }
+                .pickerStyle(.wheel)
+                
+                Text(".") // 구분점
+                
+                Picker(selection: $selectedGr, label: Text("Grams")) {
+                    ForEach(grRange, id: \.self) { gram in
+                        Text("\(gram)").tag(gram)
+                            .font(.title3)
                     }
-                    .pickerStyle(.wheel)
-                    
-                    Text("kg")
-                        .font(.title)
-                        .bold()
                 }
-                .padding(.horizontal, 16)
-                .padding(.top, 24)
+                .pickerStyle(.wheel)
                 
-                Spacer()
-                
-                HStack {
-                    Text("\(selectedKg)" + "." + "\(selectedGr)" + "kg")
-                        .font(.title)
-                        .foregroundStyle(Color.secondary)
-                        .bold()
-                        .padding(10)
-                        .background(Color.thoDisabled)
-                        .cornerRadius(10)
-                }
-                .frame(maxWidth: .infinity)
-                
-                Spacer()
+                Text("kg")
+                    .font(.title3)
+            }
+            .padding(.horizontal, 68)
+            .padding(.top, 34)
+            .onChange(of: selectedKg) { _ in
+                updateWeight()
+            }
+            .onChange(of: selectedGr) { _ in
+                updateWeight()
+                print(weight)
+            }
+            
+            
+            Spacer()
+            
             
             DailyRecordNextButton(pageNumber: $pageNumber, isActiveRecord:.constant(true), title: "다음")
             
         }
     }
+    
+    func updateWeight() {
+        weight = Double(selectedKg) + Double(selectedGr) / 10.0
+    }
 }
 
 #Preview {
-    WeightCheckView(pageNumber: .constant(11), selectedKg: .constant(50), selectedGr: .constant(0))
+    WeightCheckView(pageNumber: .constant(11), weight: .constant(50.0))
 }
