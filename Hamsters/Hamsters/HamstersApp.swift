7bb8b5c577f45fbc18d763d33148c80436f3a661
ChaeeunShin <shince0805@gmail.com>
2023-11-21 09:27:40 +0900

    [Chore/#97] 앱 시작점 수정

--- Hamsters/Hamsters/HamstersApp.swift
+++ Hamsters/Hamsters/HamstersApp.swift
@@ -13,8 +13,7 @@ import SwiftUI
 struct HamstersApp: App {
     var body: some Scene {
         WindowGroup {
-           // StartView()
-            RecordMainView()
+            StartView()
         }
     }
 }
